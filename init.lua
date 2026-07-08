eaa05b00c31d38444be3a8e47850cd2e60008708
BBIAntares <daniiltepikin8@gmail.com>
2026-07-08 16:10:28 +0000

    new: .gitignore, README.md, init.lua, lazy.lua, cmp.lua, lsp.lua, mason.lua, treesitter.lua

---
+++ init.lua
@@ -0,0 +1,12 @@
+require("config.lazy")
+
+vim.cmd.colorscheme("habamax")
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorline = true
+
+
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,36 @@
+-- lua/plugins/lsp.lua
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
+  config = function()
+    require("mason").setup()
+    local mason_lspconfig = require("mason-lspconfig")
+
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    mason_lspconfig.setup({
+      ensure_installed = { "clangd" },
+      automatic_installation = true,
+      handlers = {
+        function(server_name)
+          local server_config = {
+          capabilities = capabilities
+      }
+
+          if server_name == "clangd" then
+            server_config.cmd = { 
+                "clangd",
+                "--background-index",
+                "--clang-tidy",
+                "--query-driver=/usr/bin/g++,/usr/bin/gcc,/usr/bin/clang++,/usr/bin/clang",
+
+        }
+    end 
+
+    require("lspconfig")[server_name].setup(server_config)
+end,
+},
+})
+  end,
+}
+
